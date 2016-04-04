074c900cd647048b24ad15b414b08e1b3e0b5bb4
Thomas Weingartshofer <thomas.wein@hotmail.com>
2016-04-04 11:37:33 +0200

    Deleted unnecessary files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-src
+src folder for ros project
+
+ROS Project to mitigate malfunctions with the help of the user
--- gscam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/robot_vision_ws/src/gscam
-# Build directory: /home/chris/robot_vision_ws/src/gscam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
