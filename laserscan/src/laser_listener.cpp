ed9a9d501f4acc39d64fccc4dd3deb24e85d5013
Thomas Weingartshofer <thomas.wein@hotmail.com>
2016-04-05 15:06:26 +0200

    Added laserscan-listener

--- laserscan/src/laser_listener.cpp
+++ laserscan/src/laser_listener.cpp
@@ -1,64 +1,61 @@
 #include "ros/ros.h"
+#include "std_msgs/String.h"
 #include "sensor_msgs/LaserScan.h"
 #include "message_filters/subscriber.h"
 #include "tf/message_filter.h"
 #include "tf/transform_listener.h"
-#include "filters/filter_chain.h"
 
-class GenericLaserScanFilterNode
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void chatterCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-protected:
-  // Our NodeHandle
-  ros::NodeHandle nh_;
-
-  // Components for tf::MessageFilter
-  tf::TransformListener tf_;
-  message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
-  tf::MessageFilter<sensor_msgs::LaserScan> tf_filter_;
-
-  // Filter Chain
-  filters::FilterChain<sensor_msgs::LaserScan> filter_chain_;
-
-  // Components for publishing
-  sensor_msgs::LaserScan msg_;
-  ros::Publisher output_pub_;
-
-public:
-  // Constructor
-  GenericLaserScanFilterNode() :
-      scan_sub_(nh_, "scan_in", 50),
-      tf_filter_(scan_sub_, tf_, "base_scan", 50),
-      filter_chain_("sensor_msgs::LaserScan")
-  {
-    // Configure filter chain
-    filter_chain_.configure("filter_chain");
-
-    // Setup tf::MessageFilter for input
-    tf_filter_.registerCallback(
-        boost::bind(&GenericLaserScanFilterNode::callback, this, _1));
-    tf_filter_.setTolerance(ros::Duration(0.03));
-
-    // Advertise output
-    output_pub_ = nh_.advertise<sensor_msgs::LaserScan>("output", 1000);
-  }
-
-  // Callback
-  void callback(const sensor_msgs::LaserScan::ConstPtr& msg_in)
-  {
-    // Run the filter chain
-    filter_chain_.update (*msg_in, msg_);
-
-    // Publish the output
-    output_pub_.publish(msg_);
-  }
-};
+  ROS_INFO("I heard: [%f]", msg->ranges[1]);
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "scan_filter_node");
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "laser_listener");
 
-  GenericLaserScanFilterNode t;
-  //t.callback(t.msg_);
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /**
+   * The subscribe() call is how you tell ROS that you want to receive messages
+   * on a given topic.  This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing.  Messages are passed to a callback function, here
+   * called chatterCallback.  subscribe() returns a Subscriber object that you
+   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
+   * object go out of scope, this callback will automatically be unsubscribed from
+   * this topic.
+   *
+   * The second parameter to the subscribe() function is the size of the message
+   * queue.  If messages are arriving faster than they are being processed, this
+   * is the number of messages that will be buffered up before beginning to throw
+   * away the oldest ones.
+   */
+  ros::Subscriber sub = n.subscribe("scan_filtered", 1000, chatterCallback);
+
+  /**
+   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+   * callbacks will be called from within this thread (the main one).  ros::spin()
+   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+   */
   ros::spin();
 
   return 0;
