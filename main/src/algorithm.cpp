3aff166b3ef956e183005596bd8f7dbfdf6fc605
Thomas Weingartshofer <thomas.wein@hotmail.com>
2016-06-09 17:11:48 +0200

    completed main algorithm

--- main/src/algorithm.cpp
+++ main/src/algorithm.cpp
@@ -21,6 +21,8 @@
 #include <main/algorithm.hpp>
 #include <main/people_detection.hpp>
 
+#define SEARCH_USER 10
+
 void Algorithm::commandCallback(const std_msgs::String::ConstPtr& msg)
 {
   // to send a command over topic "command":
@@ -31,11 +33,15 @@ void Algorithm::commandCallback(const std_msgs::String::ConstPtr& msg)
   // mitigate malfunction
   // rostopic pub command std_msgs/String "error"
 
+  // solve malfunction
+  // rostopic pub command std_msgs/String "yes"
+
   const char* str = msg->data.c_str();  // define shortcut
 
   // read command
   // s for normal behaviour (h->A->h->B->h)
-  // e for error algorithm (get to last waypoint or closest user)
+  // e for error algorithm (go to last waypoint or closest user)
+  // y if help was given (go to home station and back in normal mode)
   switch(str[0])
   {
     case 's':
@@ -47,6 +53,11 @@ void Algorithm::commandCallback(const std_msgs::String::ConstPtr& msg)
     case 'E':
        ROS_ERROR("Starting failure procedure");
        status='e';
+       break;
+    case 'y':
+    case 'Y':
+       ROS_INFO("Failure solved");
+       status='h';
        break;
     default:
        ROS_ERROR("no valid command");
@@ -60,11 +71,15 @@ void Algorithm::reachedGoalCallback(const std_msgs::Int32::ConstPtr& msg)
        // rostopic pub goal_reached std_msgs/Int32 1
 
        timelastcmd = ros::Time::now().toSec();	// save time at last command
+       int attention = 1;
+       std_msgs::String text;
+       std::stringstream ss;
+       coordinates current;
 
        if(status=='a' || status=='b')  // if pointA or pointB is reached -> drive to home
        {
           statusbefore=status;	// save last waypoint
-          status='h';
+          status='h';		// set new destination
        } 
        else if(status=='h' && statusbefore=='a')  // if home is reached and robot was at pointA before -> pointB
        {
@@ -76,8 +91,45 @@ void Algorithm::reachedGoalCallback(const std_msgs::Int32::ConstPtr& msg)
        }
        else if(status=='e')  // if robot reaches user to mitigate malfunction
        {
-	    // sorted users and indizes are stored, decide if attention is given and then go to nomal behaviour or to next closest user
-       }
+	        // define timeout to wait for attention
+		ros::Time start_time = ros::Time::now();
+   		ros::Duration timeout(SEARCH_USER);
+
+		// wait for attention until timeout is over or a user gives attention
+        	while(attention==0 && ros::Time::now() - start_time < timeout && ros::ok())
+		{
+			// search attention from all users
+			for(int i=0;i<USERS;i++)
+	  	  	{
+				attention=people->getAttention(i);
+            		}
+		}
+ 
+		// if attention is give, ask for help
+            	if(attention==1)
+	    	{
+			ROS_INFO("Please, can you help me!");
+            	}
+	    	else	//if not, choose next closest user to drive to
+	    	{
+			choosen_user++;
+			if(choosen_user < people->getUserSize())  // if there are other users
+			{
+				current=people->getUserCoordinates(index_sorted[choosen_user]);
+			}
+			else	// if no more users are found, go to home station and quit the programm
+			{
+				current=home;
+				status='q';
+			}
+
+ 	        	// publish coordinates
+               	 	ss << current.x << " " << current.y;
+   	        	text.data = ss.str();
+
+                	goal_pub.publish(text);
+            	}
+       	}
 }
 
 Algorithm::Algorithm()
@@ -88,11 +140,15 @@ Algorithm::Algorithm()
     goal_pub = nh.advertise<std_msgs::String>("goal", 1);
     reachedgoal_sub = nh.subscribe("goal_reached", 1, &Algorithm::reachedGoalCallback,this);
 
+    // initialize class PeopleDetector in private variable
+    people = new PeopleDetector();
+
+    // initialize array index_sorted
     for(int i=0;i<USERS;i++)
 	index_sorted[i]=-1;
 }
 
-void Algorithm::Normal(coordinates home, coordinates pointA, coordinates pointB)
+void Algorithm::Normal()
 {
     // define variables
     std_msgs::String msg;
@@ -127,7 +183,7 @@ char Algorithm::returnStatus(void)
    return status;	   // return private variable status
 }
 
-void Algorithm::Error(PeopleDetector people, double timeerror, double timethreshold, coordinates pointA, coordinates pointB)
+void Algorithm::Error(double timeerror, double timethreshold)
 {
    // initialize variables
    std_msgs::String msg;
@@ -135,14 +191,19 @@ void Algorithm::Error(PeopleDetector people, double timeerror, double timethresh
    coordinates current;
    int size=0;
 
-   people.detectUsers();	// detect users
-   size=people.getUserSize();   // get size of users
+   // define timeout to search for users
+   ros::Time start_time = ros::Time::now();
+   ros::Duration timeout(SEARCH_USER);
 
-   // check if users are available 
-   if(size!=0 && (statusbefore=='a' || statusbefore=='b'))
+   // search for users until timeout is over or user is found
+   while(size==0 && ros::Time::now() - start_time < timeout && ros::ok())
    {
-      if(timeerror-timelastcmd<timethreshold)	// check if time since last command is lower than threshold
-      {
+   	people->detectUsers();	     // detect users
+   	size=people->getUserSize();      // get size of users
+   }
+
+   if(timeerror-timelastcmd<timethreshold && (statusbefore=='a' || statusbefore=='b'))   // check if time since last command is lower than threshold
+   {
          // write coordinates from last waypoint to drive to
          switch(statusbefore)
          {
@@ -156,20 +217,29 @@ void Algorithm::Error(PeopleDetector people, double timeerror, double timethresh
               ROS_ERROR("last commanding User not found");
               return;  
          }
-      }
-      else
-      {
-        people.getClosestUsers(&index_sorted[0]);	// write sorted indizes from users
+	 ROS_INFO("Drive to last waypoint");
+   }
+   else if(size>0)
+   {
+        people->getClosestUsers(&index_sorted[0]);	// write sorted indizes from users
        	// get User Coordinates
   	for(int i=0;i<size;i++)
   	{
-		users_sorted[i]=people.getUserCoordinates(index_sorted[i]);
+		users_sorted[i]=people->getUserCoordinates(index_sorted[i]);
    	}
 
    	// choose closest user
    	choosen_user=0;
-   	current=people.getUserCoordinates(index_sorted[choosen_user]);
-      }
+   	current=people->getUserCoordinates(index_sorted[choosen_user]);
+
+	ROS_INFO("Drive to closest user");
+   }
+   else
+   {
+        // drive to home station and quit programm, if no user is found
+	current=home;
+	status='q';
+	ROS_ERROR("No User found, drive to home station");
    }
 
    // publish coordinates
@@ -179,6 +249,19 @@ void Algorithm::Error(PeopleDetector people, double timeerror, double timethresh
    goal_pub.publish(msg);
 }
 
+void Algorithm::setRange(double range)
+{
+   people->setRange(range);	// set private varible range
+}
+
+void Algorithm::setPoints(coordinates sethome, coordinates setPointA, coordinates setPointB)
+{
+   // set waypoints to private variables
+   home=sethome;
+   pointA=setPointA;
+   pointB=setPointB;
+}
+
 int main(int argc, char **argv)
 {
   // initialize ROS
@@ -186,9 +269,8 @@ int main(int argc, char **argv)
 
   // generate objects
   Algorithm alg;
-  PeopleDetector people;
 
-  // define waypoints
+  // define waypoints and set in private variables
   coordinates home,pointA,pointB;
 
   home.x=0;
@@ -200,27 +282,33 @@ int main(int argc, char **argv)
   pointB.x=-10;
   pointB.y=-10;
 
+  alg.setPoints(home,pointA,pointB);
+
   // define time threshold and time when error occur
-  double timethreshold = 10;
+  double timethreshold = 20;
   double timeerror = 0;
 
   // define range for leg - and face data to compare to find users
-  people.setRange(0.2);
+  alg.setRange(0.2);
 
   while(ros::ok())
   {
-     // decide between normal mode and error mode
+     // decide between normal mode and error mode, if no user is helps, quit (q) the programm
      if(alg.returnStatus()=='e')
      {
-         if(timeerror==0)
+         if(timeerror==0)						// execute just one time
 	 {
          	timeerror = ros::Time::now().toSec();			// save time at error
-         	alg.Error(people,timeerror,timethreshold,pointA,pointB);// decide which user is the best to help
+         	alg.Error(timeerror,timethreshold);			// decide which user is the best to help in error mode
 	 }
      }
+     else if(alg.returnStatus()=='q')
+     {
+	return 0;
+     }
      else
-     {   timeerror = 0;
-         alg.Normal(home,pointA,pointB);
+     {   timeerror = 0;		// reset time when error appeared
+         alg.Normal();		// normal mode
      }
 
      // listen to callbacks
